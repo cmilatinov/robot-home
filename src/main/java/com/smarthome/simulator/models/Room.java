c337e42f058c5e7417b52a484decc8659dcb9e7a
alimahram <alimahram@hotmail.com>
2020-10-08 20:18:46 -0400

    Room model added

---
+++ src/main/java/com/smarthome/simulator/models/Room.java
@@ -0,0 +1,85 @@
+package com.smarthome.simulator.models;
+
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+public class Room {
+    private int id;
+    private String name;
+    private Rectangle2D dimensions;
+    private ArrayList<Window> windows;
+    private ArrayList<Door> doors;
+    private ArrayList<Light> lights;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Rectangle2D getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(Rectangle2D dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public ArrayList<Window> getWindows() {
+        return windows;
+    }
+
+    public void setWindows(ArrayList<Window> windows) {
+        this.windows = windows;
+    }
+
+    public ArrayList<Door> getDoors() {
+        return doors;
+    }
+
+    public void setDoors(ArrayList<Door> doors) {
+        this.doors = doors;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dimensions=" + dimensions +
+                ", windows=" + windows +
+                ", doors=" + doors +
+                ", lights=" + lights +
+                '}';
+    }
+
+    public Room() {
+    }
+
+    public ArrayList<Light> getLights() {
+        return lights;
+    }
+
+    public void setLights(ArrayList<Light> lights) {
+        this.lights = lights;
+    }
+
+    public Room(int id, String name, Rectangle2D dimensions, ArrayList<Window> windows, ArrayList<Door> doors, ArrayList<Light> lights) {
+        this.id = id;
+        this.name = name;
+        this.dimensions = dimensions;
+        this.windows = windows;
+        this.doors = doors;
+        this.lights = lights;
+    }
+}
