7816127c3602baa0fd228517aa300ce06dd59281
Manraj Rai <manraj14@icloud.com>
2020-10-11 11:32:43 -0400

    Added JSON simple dependency Added method to parse JSON file for house layout #6

--- src/main/java/com/smarthome/simulator/models/HouseLayout.java
+++ src/main/java/com/smarthome/simulator/models/HouseLayout.java
@@ -1,5 +1,14 @@
 package com.smarthome.simulator.models;
 
+import java.awt.geom.Rectangle2D;
+import java.io.FileReader;
+import java.io.IOException;
+import java.awt.geom.Rectangle2D;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 import java.util.ArrayList;
 
 public class HouseLayout extends Id {
@@ -39,5 +48,43 @@ public class HouseLayout extends Id {
         super();
         this.name = name;
         this.rooms = rooms;
+    }
+
+    public void parseFile()
+    {
+
+        Object obj = null;
+
+        //Parsing House-layout.json
+        try {
+            obj = new JSONParser().parse(new FileReader("house-layout.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        //Typecasting obj to JSONObject
+        JSONObject house = (JSONObject) obj;
+
+        //Getting rooms
+        JSONArray rooms = (JSONArray) house.get("rooms");
+
+        for(int i=0; i < rooms.size(); i++)
+        {
+            JSONObject room = (JSONObject) rooms.get(i);
+            String name = room.get("name").toString();
+            int nbOfWindows  = Integer.parseInt(room.get("nbOfWindows").toString());
+            int nbOfDoors  = Integer.parseInt(room.get("nbOfDoors").toString());
+            int nbOfLights  = Integer.parseInt(room.get("nbOfLights").toString());
+            double height  = Double.parseDouble(room.get("height").toString());
+            double width  = Double.parseDouble(room.get("width").toString());
+            int positionX  = Integer.parseInt(room.get("position-x").toString());
+            int positionY  = Integer.parseInt(room.get("position-y").toString());
+
+        }
+
     }
 }
