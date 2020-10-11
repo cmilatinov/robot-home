c28fc22d15b948a92eda476822a08ec6c122ee0b
Manraj Rai <manraj14@icloud.com>
2020-10-11 18:23:17 -0400

    Added methods to create rooms, windows, doors and lights from the house-layout file Added exception handling Doors can be locked or unlocked #6

--- src/main/java/com/smarthome/simulator/models/HouseLayout.java
+++ src/main/java/com/smarthome/simulator/models/HouseLayout.java
@@ -3,7 +3,6 @@ package com.smarthome.simulator.models;
 import java.awt.geom.Rectangle2D;
 import java.io.FileReader;
 import java.io.IOException;
-import java.awt.geom.Rectangle2D;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -44,6 +43,11 @@ public class HouseLayout extends Id {
         super();
     }
 
+    public HouseLayout(String name) {
+        super();
+        this.name = name;
+    }
+
     public HouseLayout(String name, ArrayList<Room> rooms) {
         super();
         this.name = name;
@@ -52,17 +56,16 @@ public class HouseLayout extends Id {
 
     public void parseFile()
     {
-
         Object obj = null;
 
-        //Parsing House-layout.json
+        //Parsing house-layout.json
         try {
             obj = new JSONParser().parse(new FileReader("house-layout.json"));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
             System.exit(0);
         } catch (ParseException e) {
-            e.printStackTrace();
+            System.out.println(e.toString());
             System.exit(0);
         }
 
@@ -72,19 +75,108 @@ public class HouseLayout extends Id {
         //Getting rooms
         JSONArray rooms = (JSONArray) house.get("rooms");
 
-        for(int i=0; i < rooms.size(); i++)
+        //Verifying if the user entered the rooms correctly
+        if(rooms == null)
         {
-            JSONObject room = (JSONObject) rooms.get(i);
-            String name = room.get("name").toString();
-            int nbOfWindows  = Integer.parseInt(room.get("nbOfWindows").toString());
-            int nbOfDoors  = Integer.parseInt(room.get("nbOfDoors").toString());
-            int nbOfLights  = Integer.parseInt(room.get("nbOfLights").toString());
-            double height  = Double.parseDouble(room.get("height").toString());
-            double width  = Double.parseDouble(room.get("width").toString());
-            int positionX  = Integer.parseInt(room.get("position-x").toString());
-            int positionY  = Integer.parseInt(room.get("position-y").toString());
-
+            System.out.println("Could not find any rooms. Make sure that house-layout.json contains a rooms array.");
+            System.exit(0);
         }
 
+        //Verifying if the house is empty
+        if(rooms.size() == 0)
+        {
+            System.out.println("House is empty. No rooms were defined.");
+            System.exit(0);
+        }
+
+        //Creating arraylist to store all rooms in the house
+        ArrayList<Room> roomsList = new ArrayList<Room>();
+
+        //Going through each room
+        for(int i=0; i < rooms.size(); i++)
+        {
+            //Getting the ith room
+            JSONObject room = (JSONObject) rooms.get(i);
+
+            String name = "";
+            int nbOfWindows = 0;
+            int nbOfDoors = 0;
+            int nbOfLights = 0;
+            float height = 0;
+            float width = 0;
+            float positionX = 0;
+            float positionY = 0;
+
+            //Getting all the information on the room
+            try {
+                name = room.get("name").toString();
+                nbOfWindows = Integer.parseInt(room.get("nbOfWindows").toString());
+                nbOfDoors = Integer.parseInt(room.get("nbOfDoors").toString());
+                nbOfLights = Integer.parseInt(room.get("nbOfLights").toString());
+                height = Float.parseFloat(room.get("height").toString());
+                width = Float.parseFloat(room.get("width").toString());
+                positionX = Float.parseFloat(room.get("position-x").toString());
+                positionY = Float.parseFloat(room.get("position-y").toString());
+            } catch(NumberFormatException e)
+            {
+                System.out.println("Invalid data type " + e.getMessage().toLowerCase() + ".");
+                System.exit(0);
+            }
+            catch(NullPointerException e)
+            {
+                System.out.println("Missing fields in house-layout.json");
+                System.exit(0);
+            }
+
+            //Saving the objects of the room in an arraylist
+            ArrayList<Window> windows = setRoomWindows(nbOfWindows);
+            ArrayList<Door> doors = setRoomDoors(nbOfDoors);
+            ArrayList<Light> lights = setRoomLights(nbOfLights);
+
+            //Setting the dimensions of the room
+            Rectangle2D.Float dimensions = new Rectangle2D.Float(positionX,positionY,width,height);
+
+            //Creating the room with the information gathered above
+            Room room1 = new Room(name,dimensions,windows,doors,lights);
+
+            //Adding the room to the list of rooms in the house
+            roomsList.add(room1);
+        }
+    }
+
+    public ArrayList<Window> setRoomWindows(int nbOfWindows)
+    {
+        //Creating the number of windows specified in the layout and storing them in a list
+        ArrayList<Window> window = new ArrayList<Window>();
+        for(int i = 0; i < nbOfWindows; i++)
+        {
+            Window window1 = new Window("Close");
+            window.add(window1);
+        }
+        return window;
+    }
+
+    public ArrayList<Door> setRoomDoors(int nbOfDoors)
+    {
+        //Creating the number of doors specified in the layout and storing them in a list
+        ArrayList<Door> door = new ArrayList<Door>();
+        for(int i = 0; i < nbOfDoors; i++)
+        {
+            Door door1 = new Door(false,false);
+            door.add(door1);
+        }
+        return door;
+    }
+
+    public ArrayList<Light> setRoomLights(int nbOfLights)
+    {
+        //Creating the number of lights specified in the layout and storing them in a list
+        ArrayList<Light> light = new ArrayList<Light>();
+        for(int i = 0; i < nbOfLights; i++)
+        {
+            Light light1 = new Light(true);
+            light.add(light1);
+        }
+        return light;
     }
 }
