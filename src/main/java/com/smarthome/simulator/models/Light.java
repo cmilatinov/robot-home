e7a9f4c24bbbdc3eca0a36fe610c37a6d3cca186
alimahram <alimahram@hotmail.com>
2020-10-14 21:15:29 -0400

    Javadoc for Light class

--- src/main/java/com/smarthome/simulator/models/Light.java
+++ src/main/java/com/smarthome/simulator/models/Light.java
@@ -1,13 +1,38 @@
 package com.smarthome.simulator.models;
 
+/**
+ * This class represents a light with its functionalities.
+ */
 public class Light extends IdentifiableObject {
+    /**
+     * Is the light on or not.
+     */
     private boolean on;
 
+    // ============================ CONSTRUCTORS ============================
+
+    /**
+     * Default constructor.
+     */
+    public Light() {
+        super();
+    }
+
+    /**
+     * Parameterized constructor.
+     * @param on Future state of the light.
+     */
     public Light(boolean on) {
         super();
         this.on = on;
     }
 
+    // ============================ OVERRIDES ============================
+
+    /**
+     * This function is meant to put all attributes of a light in a string format.
+     * @return String representation of all the current attributes of the light.
+     */
     @Override
     public String toString() {
         return "Light{" +
@@ -16,14 +41,20 @@ public class Light extends IdentifiableObject {
                 '}';
     }
 
-    public Light() {
-        super();
-    }
+    // ============================ GETTERS/SETTERS ============================
 
+    /**
+     * This function shows if a light is on or not.
+     * @return Boolean representation of it.
+     */
     public boolean isOn() {
         return on;
     }
 
+    /**
+     * This function sets the light to be on or off.
+     * @param on New state of the light.
+     */
     public void setOn(boolean on) {
         this.on = on;
     }
