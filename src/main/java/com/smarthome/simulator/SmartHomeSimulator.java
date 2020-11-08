5f1fd58ca70e015eeced4260740c2f233d603a8a
Cristian Milatinov <milatinovcristian@gmail.com>
2020-11-08 11:06:16 -0500

    last fix

--- src/main/java/com/smarthome/simulator/SmartHomeSimulator.java
+++ src/main/java/com/smarthome/simulator/SmartHomeSimulator.java
@@ -88,7 +88,7 @@ public class SmartHomeSimulator {
         pandomium.initialize();
 
         PandomiumClient client = pandomium.createClient();
-        PandomiumBrowser browser = client.loadURL("http://localhost:3000");
+        PandomiumBrowser browser = client.loadURL("http://localhost:" + WebServer.SERVER_PORT);
 
         // Create JavaScript query handler and logger
         handler = new JavaScriptQueryHandler(browser.getCefBrowser(), simulation);
