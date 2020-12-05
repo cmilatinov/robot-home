2428c4177eb764aea1319b1172bf0622079d54e0
Ian Phillips <ianjhphillips@gmail.com>
2020-12-05 18:18:13 -0500

    started hvac

--- src/main/java/com/smarthome/simulator/models/HVAC.java
+++ src/main/java/com/smarthome/simulator/models/HVAC.java
@@ -1,10 +1,11 @@
 package com.smarthome.simulator.models;
 
-
+import com.smarthome.simulator.modules.SHH;
 import com.smarthome.simulator.utils.DelayedRunnable;
 
 public class HVAC {
     private Simulation simulation;
+    private SHH shh;
     enum State {
         WORKING,
         PAUSED,
@@ -15,50 +16,60 @@ public class HVAC {
     private float pausedRate = 0f;
     private float stoppedRate = 0.05f;
 
-    public HVAC(){
+    public HVAC(Simulation simulation, SHH shh){
         super();
+        this.shh = shh;
+        this.simulation = simulation;
         this.state = State.STOPPED;
     }
+
     // ============================  Other methods  ============================
 
-    private void changeTemp(){
+    private void changeTemp(HVAC hvac, Simulation simulation){
+        if(hvac.state == State.WORKING) {
 
-
-
-        if(this.state == State.WORKING){
-            DelayedRunnable task = new DelayedRunnable() {
-
-
-                @Override
-                public long getDelay() {
-                    return 0;
-                }
-
-                @Override
-                public void setDelay(long delay) {
-
-                }
-
-                @Override
-                public boolean isPeriodic() {
-                    return false;
-                }
-
-                @Override
-                public long getPeriod() {
-                    return 0;
-                }
-
-                @Override
-                public void run() {
-
-                }
-            }
-        }else if(this.state == State.PAUSED){
-
-        }else if(this.state == State.STOPPED){
-
+            //increase temp by working amount
+        } else if(this.state == State.PAUSED) {
+            //check temp
+        }else if(this.state == State.STOPPED) {
+            //something else
         }
+    }
+
+    public void startHVAC () {
+        HVAC hvac = this;
+
+        DelayedRunnable task = new DelayedRunnable() {
+
+            private long delay = 1000000000;
+
+            @Override
+            public long getDelay() {
+                return delay;
+            }
+
+            @Override
+            public void setDelay(long delay) {
+
+            }
+
+            @Override
+            public boolean isPeriodic() {
+                return true;
+            }
+
+            @Override
+            public long getPeriod() {
+                return 1000000000;
+            }
+
+            @Override
+            public void run() {
+                changeTemp(hvac, simulation);
+            }
+        };
+
+        simulation.getDispatcher().schedule(task);
     }
 
     // ============================ GETTERS/SETTERS ============================
